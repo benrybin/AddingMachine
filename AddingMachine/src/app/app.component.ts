a17ac057595f45c26b4a360d7794ef73af4331ef
benrybinski <benrybin@gmail.com>
2019-07-08 08:53:46 -0400

    all done

--- AddingMachine/src/app/app.component.ts
+++ AddingMachine/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StylesCompileDependency } from '@angular/compiler';
+
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AddingMachine';
 }
+
+
--- AddingMachine/src/app/text-input/text-input.component.ts
+++ AddingMachine/src/app/text-input/text-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-text-input',
@@ -11,5 +12,39 @@ export class TextInputComponent implements OnInit {
 
   ngOnInit() {
   }
+  answer:number = 0;
+  calculate(first:string){
+    let ops: Array<string> = new Array;
+    let vals: Array<number>= new Array;
+    
 
+
+    let tokens: Array<string> = first.split(" ");
+    for (var token of tokens) {
+      if      (token == "(")  {continue;}
+      else if (token=="+")    ops.push(token);
+      else if (token=="-")   ops.push(token);
+      else if (token=="*")    ops.push(token);
+      else if (token=="/")    ops.push(token);
+      else if (token=="sqrt") ops.push(token);
+      else if (token==")") {
+           let op: string = ops.pop();
+          let v:number = vals.pop();
+          if      (op==("+"))    v = vals.pop() + v;
+          else if (op==("-"))    v = vals.pop() - v;
+          else if (op==("*"))    v = vals.pop() * v;
+          else if (op==("/"))    v = vals.pop() / v;
+          else if (op==("sqrt")) v = Math.sqrt(v);
+          vals.push(v);
+      }
+      else vals.push(Number.parseFloat(token));
+      
+      
+  }
+  this.answer = vals.pop(); 
+  return this.answer;
 }
+
+  }  
+
+
